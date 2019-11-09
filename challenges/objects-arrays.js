8bfd17c18cb2a15056708be5048fb9e1cb5b1178
remoo1901 <remoo1901@hotmail.com>
2019-11-08 22:50:21 -0500

    not done yet

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,7 +5,30 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
+const tyrannosaurus = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight:'7000kg',
+  length:'12m',
+  period:'Late Cretaceous'
+ 
+}
 
+const stegosaurus = {
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight:'2000kg',
+  length:'9m',
+  period: 'Late Jurassic'
+}
+
+const velociraptor = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight:'15kg',
+  length:'1.8m',
+  period:'Late Cretaceous'
+}
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -15,20 +38,23 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log("7000kg");
 
 // What was the diet of a velociraptor?
-console.log();
+console.log("carnivorous");
 
 // How long was a stegosaurus?
-console.log();
+console.log('9m');
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log('Late Cretaceous');
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+const roar =function(){
+  return "RAWERSRARARWERSARARARRRR!";
+}
+console.log(roar());
 
 
 // ==== Arrays ====
@@ -49,9 +75,13 @@ const graduates = [
 ];
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
-
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+
+const universities = graduates.map((university) =>{
+  return {"university": university.university}
+  function myFunction() {
+    university.sort();}
+});
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
